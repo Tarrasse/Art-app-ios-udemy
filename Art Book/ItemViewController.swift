aba39c2c900fcd25e0b76ec5c9f05a035d198270
Mahmoud El-Tarrasse <tarrasse@Mahmouds-Device.local>
2017-08-16 13:50:20 +0200

    first commit - fully implemented

--- Art Book/ItemViewController.swift
+++ Art Book/ItemViewController.swift
@@ -7,13 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemViewController: UIViewController {
+class ItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var artistTextField: UITextField!
+    @IBOutlet weak var planingYear: UITextField!
+    
+    var chosenPainting = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        let gestureRecognizer  = UITapGestureRecognizer(target: self, action: #selector(ItemViewController.selectImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        
+        if chosenPainting != ""{
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            let context = appDelegate?.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.chosenPainting)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context?.fetch(fetchRequest)
+                if (results?.count)! > 0{
+                    for result in results as! [NSManagedObject]{
+                        if let name  = result.value(forKey: "name") as? String{
+                             nameTextField.text = name
+                            
+                        }
+                        if let year = result.value(forKey: "year") as? Int{
+                            planingYear.text = String(year)
+                        }
+                        
+                        if let artist = result.value(forKey: "artist")as? String{
+                            artistTextField.text = artist
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data{
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                }
+            }catch{
+                
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +68,46 @@ class ItemViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    @IBAction func save(_ sender: UIButton) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let context = appDelegate?.persistentContainer.viewContext
+        
+        let newArt = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context!)
+        newArt.setValue(nameTextField.text, forKey: "name")
+        newArt.setValue(artistTextField.text, forKey: "artist")
+        if let intyear = Int(planingYear.text!){
+            newArt.setValue(intyear, forKey: "year")
+        }
+        
+        
+        let data = UIImageJPEGRepresentation(imageView.image!, 0.5)
+        newArt.setValue(data, forKey: "image")
+        
+        do{
+            try context?.save()
+            navigationController?.popViewController(animated: true)
+
+            print("saved")
+        }catch{
+            print("error saving the data")
+        }
+
+    }
+
+
+    func selectImage(){
+        let picker  = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
--- Art Book/TableViewController.swift
+++ Art Book/TableViewController.swift
@@ -7,18 +7,65 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
+    let cellId = "id"
+    
+    var names = [String]()
+    var years = [Int]()
+    var artists = [String]()
+    var images = [UIImage]()
+    
+    var selectedPainting = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        reviveData()
     }
+    
+    private func reviveData(){
+        names.removeAll()
+        years.removeAll()
+        artists.removeAll()
+        images.removeAll()
+        
+        
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let context = appDelegate?.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        
+        fetchRequest.returnsObjectsAsFaults = false
+        do{
+            let results = try context?.fetch(fetchRequest)
+            if (results?.count)! > 0{
+                for result in (results as? [NSManagedObject])!{
+                    if let name = result.value(forKey: "name") as? String{
+                        names.append(name)
+                    }
+                    if let year = result.value(forKey: "year") as? Int{
+                        years.append(year)
+                    }
+                    
+                    if let artist = result.value(forKey: "artist")as? String{
+                        artists.append(artist)
+                    }
+                    if let imageData = result.value(forKey: "image") as? Data{
+                        let image = UIImage(data: imageData)
+                        images.append(image!)
+                        
+                    }
+                }
+            }
+        }catch{
+            print("retch error")
+        }
+        
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -28,24 +75,29 @@ class TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return images.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        cell.detailTextLabel?.text = artists[indexPath.row]
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selectedPainting = names[indexPath.row]
+        performSegue(withIdentifier: "toCreate", sender: nil)
+    }
+    
+    
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -82,14 +134,19 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "toCreate"{
+            if let destVC = segue.destination as? ItemViewController{
+                print(self.selectedPainting)
+                destVC.chosenPainting = selectedPainting
+            }
+        }
     }
-    */
+    
 
 }
